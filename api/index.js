a0ae838b11735ca0d77b626832d13bf2881470ad
justme_herza <157363409+herzonly@users.noreply.github.com>
2025-04-10 17:58:19 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static('../public'));
 
 const upload = multer({ dest: 'uploads/' });
 let model;
